2e833c71b13843f984219b2b2caf5865876e6521
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-09-12 18:51:41 -0500

    WebClientTest - Error Handler, Query Param, Response Detail, and Basic Auth Scenarios Added.

---
+++ src/main/java/com/learn_everyday/webflux_learning/chapter7/dto/Calculator.java
@@ -0,0 +1,4 @@
+package com.learn_everyday.webflux_learning.chapter7.dto;
+
+public record Calculator(Integer first, Integer second, String operation, double result) {
+}
--- src/test/java/com/learn_everyday/webflux_learning/chapter7/WebClientTest.java
+++ src/test/java/com/learn_everyday/webflux_learning/chapter7/WebClientTest.java
@@ -1,8 +1,14 @@
 package com.learn_everyday.webflux_learning.chapter7;
 
+import com.learn_everyday.webflux_learning.chapter7.dto.Calculator;
 import com.learn_everyday.webflux_learning.chapter7.dto.Product;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -11,10 +17,13 @@ import java.util.Map;
 
 public class WebClientTest {
 
+    private static final Logger log = LoggerFactory.getLogger(WebClientTest.class);
+
     AbstractWebClient abstractWebClient = new AbstractWebClient();
 
     private final WebClient webClient = abstractWebClient.createWebClient();
-    private final WebClient webClientWithHeader = abstractWebClient.createWebClient(header -> header.defaultHeader("caller-id", "order-service"));
+    private final WebClient webClientWithHeader = abstractWebClient.createWebClient(header -> header.defaultHeader("java", "secret"));
+        private final WebClient webClientWithAuth = abstractWebClient.createWebClient(header -> header.defaultHeaders(auth -> auth.setBasicAuth("java", "secret")));
 
     @Test
     public void getTest() throws InterruptedException {
@@ -121,6 +130,97 @@ public class WebClientTest {
         this.webClientWithHeader.get()
                 .uri("/lec04/product/{id}", 1)
                 .headers(httpHeaders -> httpHeaders.setAll(map))
+                .retrieve()
+                .bodyToMono(Product.class)
+                .doOnNext(abstractWebClient.print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void errorHandlerTest() {
+
+        this.webClient.get()
+                .uri("/lec05/calculator/{a}/{b}", 20, 10)
+                .header("operation", "@")
+                .retrieve()
+                .bodyToMono(Calculator.class)
+                .doOnError(WebClientResponseException.class, ex -> log.info("{}", ex.getResponseBodyAs(ProblemDetail.class)))
+                .onErrorReturn(WebClientResponseException.InternalServerError.class, new Calculator(0, 0, null, 0.0))
+                .onErrorReturn(WebClientResponseException.BadRequest.class, new Calculator(0, 0, null, -1.0))
+                .doOnNext(abstractWebClient.print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void retrieveResponseDetailsTest() {
+
+        this.webClient.get()
+                .uri("/lec05/calculator/{a}/{b}", 20, 10)
+                .header("operation", "+")
+                .exchangeToMono(this::decodeResponse)
+                .doOnNext(abstractWebClient.print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    private Mono<Calculator> decodeResponse(ClientResponse response) {
+//        response.cookies();
+//        response.headers();
+        log.info("Status Code: {}", response.statusCode());
+        if (response.statusCode().isError()) {
+            return response.bodyToMono(ProblemDetail.class)
+                    .doOnNext(problemDetail -> log.info("{}", problemDetail))
+                    .then(Mono.empty());
+        }
+        return response.bodyToMono(Calculator.class);
+    }
+
+
+    @Test
+    public void queryParamTest() {
+        var path = "/lec06/calculator";
+        var query = "first={first}&second={second}&operation={operation}";
+        this.webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(path).query(query).build(10, 20, "+"))
+                .retrieve()
+                .bodyToMono(Calculator.class)
+                .doOnNext(abstractWebClient.print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void queryParamUsingMapTest() {
+        var path = "/lec06/calculator";
+        var query = "first={first}&second={second}&operation={operation}";
+        var map = Map.of("first", 10,
+                "second", 20,
+                "operation", "+");
+        this.webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(path).query(query).build(map))
+                .retrieve()
+                .bodyToMono(Calculator.class)
+                .doOnNext(abstractWebClient.print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void basicAuthTest() {
+        this.webClientWithAuth.get()
+                .uri("/lec07/product/{id}", 1)
                 .retrieve()
                 .bodyToMono(Product.class)
                 .doOnNext(abstractWebClient.print())
